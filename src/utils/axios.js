ea86f5f6a96848f9e03dbbc6b4744279c9aae457
Shoaib Ansari <shoaib.ansari8600@gmail.com>
2024-11-04 12:32:00 +0530

    update baseURL in axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // Change this to match your backend URL and port
-  baseURL: "http://localhost:5000/api",
-  // Add withCredentials for CORS requests with credentials
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://exit-poll-backend-ooax.onrender.com/api",
+
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
