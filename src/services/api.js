392e7938673d58af83bb653279ef5b262b959201
Shoaib Ansari <shoaib.ansari8600@gmail.com>
2024-11-06 15:41:59 +0530

    added wallpaper

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
-// const API_URL = "https://exit-poll-backend-ooax.onrender.com/api";
+// const API_URL = "http://localhost:5000/api";
+const API_URL = "https://exit-poll-backend-ooax.onrender.com/api";
 
 export const getCities = async () => {
   try {
